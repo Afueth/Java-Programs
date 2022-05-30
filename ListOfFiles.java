51f183885f337adad56e093dd0be9f3972dbbc88
Afueth Thomas <97304915+Afueth@users.noreply.github.com>
2022-05-30 15:36:03 +0530

    Add files via upload
    Java Program to list the sub directories and files in a given directory

---
+++ ListOfFiles.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+public class ListOfFiles {
+	  public static void main(String args[]) throws IOException {
+      File directoryPath = new File("D:\\Example");
+      String contents[] = directoryPath.list();
+      System.out.println("List of files and directories in the specified directory:");
+      for(int i=0; i<contents.length; i++) {
+         System.out.println(contents[i]);
+      }
+   }
+}
